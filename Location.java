efe111f9a5ee5110fc932be3aafa8d2c61ea5903
Andrew Hochmuth <andhochmuth@gmail.com>
2013-11-20 17:24:39 -0600

    Added Location Class

---
+++ Location.java
@@ -0,0 +1,26 @@
+
+public class Location {
+
+	private String region = "Forest of Goluth";
+	private String location = "Temple of Thimvel";
+	private String room = "Entrance Way";
+	
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getRoom() {
+		return room;
+	}
+	public void setRoom(String room) {
+		this.room = room;
+	}
+}
