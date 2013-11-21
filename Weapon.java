866172966c122ff6d10f53d0b58d994f100ceb15
Andrew Hochmuth <andhochmuth@gmail.com>
2013-11-20 18:03:30 -0600

    Added Weapons Class
    All things weapons here!

---
+++ Weapon.java
@@ -0,0 +1,17 @@
+
+public class Weapon {
+	private String Name;
+	private int Damage;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getDamage() {
+		return Damage;
+	}
+	public void setDamage(int damage) {
+		Damage = damage;
+	}
+}
